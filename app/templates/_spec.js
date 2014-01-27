6628d09ca538b19651caf8034b3831acde00ca9e
Phillip Alexander <git@phillipalexander.io>
2014-01-26 20:00:04 -0800

    Add templates for index, spec, and src files

---
+++ app/templates/_spec.js
@@ -0,0 +1,20 @@
+/* global <%= algorithm %>, describe, it, expect, should */
+
+describe('<%= algorithm %>()', function () {
+  'use strict';
+
+  it('exists', function () {
+    expect(<%= algorithm %>).to.be.a('function')
+
+  });
+
+  it('does something', function () {
+    expect(true).to.equal(false);
+  });
+
+  it('does something else', function () {
+    expect(true).to.equal(false);
+  });
+
+  // Add more assertions here
+});
---
+++ app/templates/_src.js
@@ -0,0 +1,6 @@
+/*! <%= algorithm %> v0.0.0 - MIT license */
+'use strict';
+
+var <%= algorithm %> = function() () {
+  // your code goes here
+}
