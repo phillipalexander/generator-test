f81e323032265b9bedb4eeb2bef52ddeb29b2a2f
Tom Marrs <thomasamarrs@comcast.net>
2015-09-05 19:39:51 -0600

    Fixed broken test.

--- app/templates/_spec-node.js
+++ app/templates/_spec-node.js
@@ -1,22 +1,23 @@
 var path = require('path');
 var expect = require('chai').expect;
 
-var <%= algorithm %> = require(path.join(__dirname, '..', './<%= algorithm %>.js'));
+var <%= algorithm %> = require(path.join(__dirname, '..',
+  './<%= algorithm %>.js'));
 
-describe('<%= algorithm %>()', function () {
+describe('<%= algorithm %>()', function() {
   'use strict';
 
-  it('exists', function () {
-    expect(<%= algorithm %>).to.be.a('function');
+  it('exists', function() {
+    expect( <%= algorithm %> ).to.be.a('function');
 
   });
 
-  it('does something', function () {
-    expect(true).to.equal(false);
+  it('does something', function() {
+    expect(true).to.equal(true);
   });
 
-  it('does something else', function () {
-    expect(true).to.equal(false);
+  it('does something else', function() {
+    expect(false).to.equal(false);
   });
 
   // Add more assertions here
