a2ecd45e5a1ad55efed6fc779d9866e91e0792b9
CoyoteBringsFire <aumkara@gmail.com>
2015-11-06 22:48:48 -0800

    updated templates to conditionally using should or chai.expect for assertions

--- app/templates/_spec-node.js
+++ app/templates/_spec-node.js
@@ -1,5 +1,13 @@
 var path = require('path');
+<% switch(assertstyle) {
+  case "expect":
+  %>
 var expect = require('chai').expect;
+  <% break;
+  case "should":
+  %>
+var should = require('should');
+  <% break;  } %>
 
 var <%= algorithm %> = require(path.join(__dirname, '..', './<%= algorithm %>.js'));
 
@@ -7,16 +15,33 @@ describe('<%= algorithm %>()', function () {
   'use strict';
 
   it('exists', function () {
-    expect(<%= algorithm %>).to.be.a('function');
-
+    <% switch( assertstyle ) { 
+    case "expect": %>
+      expect(<%= algorithm %>).to.be.a('function');
+    <% break; 
+    case "should":%>
+      (typeof <%= algorithm %>).should.not.equal('function');
+    <% break; } %>
   });
 
   it('does something', function () {
-    expect(true).to.equal(false);
+    <% switch( assertstyle ) { 
+    case "expect": %>
+      expect(true).to.equal(false);
+    <% break; 
+    case "should":%>
+      true.should.not.be.ok();
+    <% break; } %>
   });
 
   it('does something else', function () {
-    expect(true).to.equal(false);
+    <% switch( assertstyle ) { 
+    case "expect": %>
+      expect(true).to.equal(false);
+    <% break; 
+    case "should":%>
+      true.should.not.be.ok();
+    <% break; } %>
   });
 
   // Add more assertions here
